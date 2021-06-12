77cfc0750e57c976f9065321a99ad40710584e86
dough10 <doughten.jimi@gmail.com>
2021-06-12 06:19:44 -0500

    U: formatting, V: 1.0.0

--- lib/encoder.py
+++ lib/encoder.py
@@ -2,13 +2,14 @@ from lib.rotary_irq_rp2 import RotaryIRQ
 
 class ENCODER: 
   def __init__(self, pins, leds, __function_state):
-    self.__knob = RotaryIRQ(pin_num_clk=pins[0],
-              pin_num_dt=pins[1],
-              min_val=0,
-              max_val=65025,
-              reverse=False,
-              range_mode=RotaryIRQ.RANGE_UNBOUNDED,
-              pull_up=True)
+    self.__knob = RotaryIRQ(
+      pin_num_clk=pins[0],
+      pin_num_dt=pins[1],
+      min_val=0,
+      max_val=65025,
+      reverse=False,
+      range_mode=RotaryIRQ.RANGE_UNBOUNDED,
+      pull_up=True)
     self.__value = self.__knob.value()
     self.__function_state = __function_state
     self.LEDS = leds
